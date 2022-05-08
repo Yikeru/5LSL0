79bd2a08e023f85be2a2403443b363cde670b72c
Ana <a.m.risnoveanu@student.tue.nl>
2022-05-08 18:06:02 +0200

    Add algorithms for GD, Newton, RLS

---
+++ GD.py
@@ -0,0 +1,41 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Extract data from csv file
+file = open(r"C:\Users\20167271\Desktop\ML for signal processing\A1\assignment1_data.csv")
+csvreader = csv.reader(file)
+header = ['x[k]', 'y[k]']  # next(csvreader)
+print(header)
+rows = []
+for row in csvreader:
+    rows.append(row)
+file.close()
+
+
+# Initialize filter
+lambda_max = 1/7
+alpha = np.random.randn(1) * lambda_max  # ensures convergence
+N = len(rows)  # number of samples
+D = 3 #len(rows[0][0])  # input dimension
+w0 = np.random.randn(D,1)  # initialize weights from random distribution
+ryx = np.array([[1],[5.3],[-3.9]])
+Rx = np.array([[5, -1, -2], [-1, 5, -1], [-2, -1, 5]])
+
+
+# Implement algorithm
+whist = [w0.tolist()]
+for i in range(N):
+    w1 = w0 + 2*alpha*(ryx - np.matmul(Rx, w0))
+    whist.append(w1.tolist())
+    w0 = w1
+
+whist = np.array(whist)
+p = plt.plot(whist[:,0], whist[:,1], '-o')
+plt.plot(whist[0,0], whist[0,1], 'r-o', markeredgewidth = 2)
+plt.plot(whist[1001,0], whist[1001,1], 'g-o', markeredgewidth = 2)
+plt.xlabel('$w_{0}$')
+plt.ylabel("$w_{1}$")
+plt.title("Weight convergence with GD")
+plt.savefig(r"C:\Users\20167271\Desktop\ML for signal processing\A1\ex122f.png")
+plt.show()
---
+++ RLS.py
@@ -0,0 +1,52 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Extract data from csv file
+file = open(r"C:\Users\20167271\Desktop\ML for signal processing\A1\assignment1_data.csv")
+csvreader = csv.reader(file)
+header = ['x[k]', 'y[k]']  # next(csvreader)
+print(header)
+rows = []
+for row in csvreader:
+    rows.append(row)
+file.close()
+N = len(rows)  # number of samples
+#xs = [rows[r][0] for r in range(N)]
+#ys = [rows[r][1] for r in range(N)]
+rows = np.array(rows, dtype=np.float64)
+xs = rows[:,0]
+ys = rows[:,1]
+
+
+# Initialize filter
+gamma = 1-(1e-3)
+ryx = np.array([[0],[0],[0]])
+Rxinv = np.identity(3)*1e-3
+RxInvList = [Rxinv]
+ryxList = [ryx]
+wo = []
+
+# Implement algorithm
+for i in range(N-3):
+    x = xs[i:i+3].reshape((3,1))
+    y = ys[i+2]
+    g = (Rxinv @ x) / (gamma**2 + (np.transpose(x) @ (Rxinv @ x)))
+    Rxinv = gamma**-2 * (Rxinv - g @ (np.transpose(x) @ Rxinv))
+    RxInvList.append(Rxinv)
+    ryx = gamma**2 * ryx + x * y  # ???????????????????????????????Is this the right y or should it be y[i]
+    ryxList.append(ryx)
+    w = Rxinv @ ryx
+    wo.append(w)
+
+
+# Plot
+whist = np.array(wo)
+p = plt.plot(whist[:,0], whist[:,1], '-o')
+plt.plot(whist[0,0], whist[0,1], 'r-o', markeredgewidth = 2)
+plt.plot(whist[1001,0], whist[1001,1], 'g-o', markeredgewidth = 2)
+plt.xlabel('$w_{0}$')
+plt.ylabel("$w_{1}$")
+plt.title("Weight convergence with Newton")
+plt.savefig(r"C:\Users\20167271\Desktop\ML for signal processing\A1\ex132m.png")
+plt.show()
