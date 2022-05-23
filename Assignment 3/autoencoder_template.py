b305bdc86986fdaedf7c41eae4e9170f48174c3c
Yikeru <jexel_231@hotmail.com>
2022-05-23 12:07:21 +0200

    Started on model

--- Assignment 3/autoencoder_template.py
+++ Assignment 3/autoencoder_template.py
@@ -1,13 +1,21 @@
 # %% imports
 import torch
 import torch.nn as nn
+from torch.nn.modules.activation import ReLU
 
 # %%  Encoder
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         # create layers here
-        
+            nn.Conv2d(1, 4, kernel_size=(3,3), stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(),
+            
+            nn.Conv2d(1, 4, kernel_size=(3,3), stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d
+
     def forward(self, x):
         # use the created layers here
         h = x
@@ -18,7 +26,13 @@ class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
         # create layers here
-        
+            nn.ConvTranspose2d(),
+            nn.ReLU(),
+            nn.Upsample(),
+
+            nn.ConvTranspose2d(),
+            nn.ReLU(),
+            nn.Upsample()
     def forward(self, h):
         # use the created layers here
         r = h
