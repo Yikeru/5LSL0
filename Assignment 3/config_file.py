1d85aa629f2ff6c04ef5233247563bc8082c7d10
Yikeru <jexel_231@hotmail.com>
2022-05-25 17:00:21 +0200

    Added plotting function and N-nearest Neighbor

--- Assignment 3/config_file.py
+++ Assignment 3/config_file.py
@@ -1,1 +1,6 @@
-data_loc = r"C:\Users\20167271\Desktop\ML for signal processing\5LSL0-GIT\Assignment 3\data"
+# data_loc = r"C:\Users\20167271\Desktop\ML for signal processing\5LSL0-GIT\Assignment 3\data"
+import os
+data_loc = os.getcwd()
+# print(data_loc)
+data_loc = data_loc + '\Assignment 3\data'
+# print(data_loc)
