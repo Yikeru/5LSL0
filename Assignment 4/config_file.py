22c8dba6eeaff70f55e74ff5b1aef617c44703e6
Yikeru <jexel_231@hotmail.com>
2022-06-09 17:23:58 +0200

    Set up Assignment 4

---
+++ Assignment 4/config_file.py
@@ -0,0 +1,7 @@
+# data_loc = r"C:\Users\20167271\Desktop\ML for signal processing\5LSL0-GIT\Assignment 3\data"
+import os
+data_loc = os.getcwd()
+# print(data_loc)
+data_loc = data_loc + '\Assignment 3\data'
+batch_size = 64
+# print(data_loc)
